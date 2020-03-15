54ae9e8c744b30939b2f83ed9020a934f31672cb
Arun Patel <patelarun741@gmail.com>
2020-03-15 17:59:26 +0530

    add auth0 authenticator

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Navbar from 'components/Navbar';
 
 function App(props) {
   return (
     <div className="App">
+      <header>
+        <Navbar />
+      </header>
       <h2 className='mb-5 p-2'>Web Gallery</h2>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,14 +3,33 @@ import 'bootstrap/dist/css/bootstrap.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App';
+import App from 'containers/App';
+import { Auth0Provider } from "./react-auth0-spa";
 
+import config from './auth-config.json';
 import { Provider } from 'react-redux';
 import store from './store';
 
+import history from "utils/history";
+
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
 const Root = () => (
   <Provider store={store}>
-    <App />
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <App />
+    </Auth0Provider>
   </Provider>
 );
 
